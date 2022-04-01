55787fe954126e860657caa12af97f8be848ab76
SudhirPC <sudhirchavhan100@gmail.com>
2022-04-01 17:39:54 +0530

    first commit

--- src/Schema/classroomSchema.js
+++ src/Schema/classroomSchema.js
@@ -6,7 +6,7 @@ const classroomSchema = new mongoose.Schema(
     title: { type: String, required: true },
     description: { type: String, required: true },
     Price: { type: Number, required: true },
-    strikethrough_Price: { type: Number, required: true },
+    strikethrough_Price: { type: String, required: true },
     know: { type: String, required: true },
     emi: { type: String, required: true },
     incart:{ type: Number, required: false },
